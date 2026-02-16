f61ed8eb330a5e9587d362820d558f1da6ec8465
Khang Lê <khenm@Khangs-MacBook-Pro.local>
2026-02-16 22:56:44 +0900

    fix: update setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 setup(
     name='dyt_cuda',
     version='0.1.0',
+    packages=find_packages(include=['dyt']),
     ext_modules=[
         CUDAExtension(
             name="dyt_cuda", 
@@ -19,4 +20,5 @@ setup(
         )
     ],
     cmdclass={"build_ext": BuildExtension},
+    install_requires=['torch']
 )
